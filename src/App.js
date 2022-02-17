629a44a83475fca119c0ff49253166c873ae8c88
Tommi Mäkelä <tmmi.makela@outlook.com>
2022-02-17 13:12:48 +0200

    feat: Add a feature to get all pokemons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
 /**
  * Module dependencies
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import pokemonService from './services/pokemon-api';
 
 /**
  * Main component
  */
 const App = () => {
+
+  /**
+   * Component states
+   */
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  /**
+   * Retrieving data from API and initializing pokemons
+   */
+  useEffect(() => {
+    getAllPokemons();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const createPokemonObject = async (result) => {
+
+    result.forEach((pokemon) => {
+
+      pokemonService
+        .getPokemon(pokemon)
+        .then(returnedPokemon => {
+          setAllPokemons((currentList) => [...currentList, returnedPokemon]);
+        })
+        .catch(error => {
+          console.log('Error in createPokemonObject: ', error);
+        });
+    });
+
+  };
+
+  const getAllPokemons = async () => {
+
+    let data = await pokemonService
+      .getAllPokemons()
+      .then(initialData => {
+        return initialData.results;
+      })
+      .catch(error => {
+        console.log('Error in getAllPokemons: ', error);
+      });
+
+    createPokemonObject(data);
+  };
+
+  //console.log('pokemons: ', allPokemons);
+
   return (
     <div>
-      Hello world!
+
+      {allPokemons.map((obj, i) =>
+        <p key={i} > {obj.name} </p>
+      )}
+
     </div>
   );
 };
---
+++ src/services/pokemon-api.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+const url = 'https://pokeapi.co/api/v2/pokemon';
+
+const getAllPokemons = async () => {
+
+  let response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+  });
+
+  if (response.status !== 200) {
+    let data = response.json();
+    throw new Error('Error: ', data.error);
+  }
+
+  let data = await response.json();
+
+  response = await fetch(`${url}/?limit=${data.count}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+  });
+
+  if (response.status !== 200) {
+    let data = response.json();
+    throw new Error('Error: ', data.error);
+  }
+
+  return await response.json();
+
+};
+
+const getPokemon = async (pokemon) => {
+
+  let response = await fetch(`${url}/${pokemon.name}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+  });
+
+  if (response.status !== 200) {
+    let data = response.json();
+    throw new Error('Error: ', data.error);
+  }
+
+  return await response.json();
+
+};
+
+export default { getAllPokemons, getPokemon };
+
